6be4a6ffd62f5014b8d6c37799c5b71a0a1dd836
Derek Bruckner <derek.bruckner@detroitlabs.com>
2022-06-01 09:34:29 -0400

    Add auto-reloading to server, similar to nodemon for a NodeJS app.

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 
 - cargo new server
 - add dependencies to `Cargo.toml`
+- add Auto Reloading: `cargo install systemfd cargo-watch`
+
+  - add `listenfd = "0.3"` to dependencies
+
+- To start server: **systemfd --no-pid -s h
+  ttp::8080 -- cargo watch -x run**
--- server/src/main.rs
+++ server/src/main.rs
@@ -4,9 +4,10 @@ extern crate diesel;
 use actix_web::{get, middleware, post, web, App, Error, HttpResponse, HttpServer, Responder};
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
-use uuid::Uuid;
+use listenfd::ListenFd;
+// use uuid::Uuid;
 
-type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
+// type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -15,6 +16,7 @@ async fn index() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let mut listenfd = ListenFd::from_env();
     dotenv::dotenv().ok();
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
@@ -27,15 +29,20 @@ async fn main() -> std::io::Result<()> {
 
     log::info!("starting HTTP server at http://localhost:8080");
 
-    // Start HTTP server
-    HttpServer::new(move || {
+    // Declare HTTP server
+    let mut server = HttpServer::new(move || {
         App::new()
             // set up DB pool to be used with web::Data<Pool> extractor
             // .app_data(web::Data::new(pool.clone()))
             // .wrap(middleware::Logger::default())
             .service(index)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    });
+
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None => server.bind("127.0.0.1:8080")?,
+    };
+
+    // Start HTTP server
+    server.run().await
 }
